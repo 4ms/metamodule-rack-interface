8a9f8fcacd151eaefdd67bc74ead12fb38dc1b65
Dan Green <danngreen1@gmail.com>
2024-04-09 16:27:17 -0700

    Interface lib is agnostic to plugin vs. built-in

--- interface.cmake
+++ interface.cmake
@@ -1,12 +1,5 @@
 add_library(metamodule-rack-interface INTERFACE)
 
-target_compile_options(metamodule-rack-interface INTERFACE
-	-shared
-	-fPIC
-	-nostartfiles
-	-nostdlib
-)
-
 target_include_directories(metamodule-rack-interface INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/include
     ${CMAKE_CURRENT_LIST_DIR}/dep/include

a54534b09f9b8790528d11de2e9f40d71de49137
Dan Green <danngreen1@gmail.com>
2024-04-11 15:56:43 -0700

    Rack SDK headers are SYSTEM headers

--- interface.cmake
+++ interface.cmake
@@ -1,6 +1,6 @@
 add_library(metamodule-rack-interface INTERFACE)
 
-target_include_directories(metamodule-rack-interface INTERFACE
+target_include_directories(metamodule-rack-interface SYSTEM INTERFACE
     ${CMAKE_CURRENT_LIST_DIR}/include
     ${CMAKE_CURRENT_LIST_DIR}/dep/include
 )

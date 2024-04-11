24a85944129a2beb9f6b50137279290ed82c42c4
Dan Green <danngreen1@gmail.com>
2024-04-11 15:56:01 -0700

    Treat dynamic_cast as reinterpret_cast

--- include/metamodule/dynamic_cast.hpp
+++ include/metamodule/dynamic_cast.hpp
@@ -1,12 +1,10 @@
 #pragma once
 
-namespace rack
-{
+namespace rack {
 #define dynamic_cast rack::stub_dynamic_cast
 
-template<typename T>
-T stub_dynamic_cast(void *) {
-	return nullptr;
+template <typename T> T stub_dynamic_cast(void *p) {
+  return reinterpret_cast<T>(p);
 }
 
 } // namespace rack

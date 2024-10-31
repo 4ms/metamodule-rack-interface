188ccde1f67424f329d84277ba4c7fb137859bbd
Dan Green <danngreen1@gmail.com>
2024-10-30 22:46:11 -0700

    Revert "Use CHECKMARK and RIGHT_ARROW symbols that are in MM fonts"
    This reverts commit eaa47e0db032ceecbbdd6d9da71992fdf39b540b.

--- include/ui/common.hpp
+++ include/ui/common.hpp
@@ -6,13 +6,11 @@
 #include <common.hpp>
 
 /** Useful for menu items with a "true" boolean state */
-//#define CHECKMARK_STRING "✔"
-#define CHECKMARK_STRING ""
+#define CHECKMARK_STRING "✔"
 #define CHECKMARK(_cond) ((_cond) ? CHECKMARK_STRING : "")
 
 /** Useful for menu items that open a sub-menu */
-//#define RIGHT_ARROW "▸"
-#define RIGHT_ARROW ""
+#define RIGHT_ARROW "▸"
 
 namespace rack::ui
 {

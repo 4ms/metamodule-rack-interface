7e6ce56586350026e4795fce4a7cd665996a92c5
Dan Green <danngreen1@gmail.com>
2025-01-08 16:52:34 -0800

    Add default visibility to `init()` symbol

--- include/plugin/callbacks.hpp
+++ include/plugin/callbacks.hpp
@@ -7,7 +7,7 @@
 Use this to save `plugin` to a global variable and add Models to it.
 Required in plugins.
 */
-extern "C"
+extern "C" __attribute__((__visibility__("default")))
 void init(rack::plugin::Plugin* plugin);
 
 /** Called before your plugin library is unloaded.

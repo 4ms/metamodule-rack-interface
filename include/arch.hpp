12385d96077f709889bdf48e025c6477abf34f1c
Dan Green <danngreen1@gmail.com>
2025-02-13 07:40:09 -0800

    Format/cleanup more headers

--- include/arch.hpp
+++ include/arch.hpp
@@ -1,20 +1,21 @@
 #pragma once
 
 // Check CPU
-#if defined __x86_64__
-	#define ARCH_X64 1
-#endif
-#if defined __aarch64__
-	#define ARCH_ARM64 1
+#ifdef METAMODULE
+#define ARCH_CA7 1
+#elif defined __x86_64__
+#define ARCH_X64 1
+#elif defined __aarch64__
+#define ARCH_ARM64 1
 #endif
 
 // Check OS
-#if defined _WIN32
-	#define ARCH_WIN 1
-#endif
-#if defined __APPLE__
-	#define ARCH_MAC 1
-#endif
-#if defined __linux__
-	#define ARCH_LIN 1
+#ifdef METAMODULE
+#define ARCH_METAMODULE 1
+#elif defined _WIN32
+#define ARCH_WIN 1
+#elif defined __APPLE__
+#define ARCH_MAC 1
+#elif defined __linux__
+#define ARCH_LIN 1
 #endif
